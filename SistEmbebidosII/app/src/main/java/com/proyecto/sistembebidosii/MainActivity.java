0acbce6d9ca500aae87411e68548e60e54e6c220
FanniiveeGa <x_fanny_x@live.com.mx>
2015-02-21 07:07:38 -0600

    Cambios actividad 2

--- SistEmbebidosII/app/src/main/java/com/proyecto/sistembebidosii/MainActivity.java
+++ SistEmbebidosII/app/src/main/java/com/proyecto/sistembebidosii/MainActivity.java
@@ -26,11 +26,30 @@ public class MainActivity extends Activity {
         return true;
         }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            System.out.println("Se presionó configuración");
+            return true;
+        }else if (id == R.id.action_search)
+            System.out.println("Se presionó la búsqueda");
+
+        return super.onOptionsItemSelected(item);
+    }
+
         public void lanzar (View view) {
             Intent i = new Intent (this, Activity2.class);
             startActivity(i);
 
 
         }
+
+
 
 }
