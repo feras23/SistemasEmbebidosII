a9cd752a2adc79c9156d4fb5b6a15eb4230ddf73
Fernando Esteban Perez Mancilla <feras_94@hotmail.com>
2015-02-21 07:59:28 -0600

    ActionBar

--- SistEmbebidosII/app/src/main/java/com/proyecto/sistembebidosii/Activity2.java
+++ SistEmbebidosII/app/src/main/java/com/proyecto/sistembebidosii/Activity2.java
@@ -1,13 +1,12 @@
 package com.proyecto.sistembebidosii;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 
 
 
-public class Activity2 extends Activity{
+public class Activity2 extends ActionBarActivity {
 
 
 
@@ -16,12 +15,8 @@ public class Activity2 extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2);
     }
-
-
     public boolean onOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.menu_activity2, menu);
-
         return true;
     }
 
--- SistEmbebidosII/app/src/main/java/com/proyecto/sistembebidosii/MainActivity.java
+++ SistEmbebidosII/app/src/main/java/com/proyecto/sistembebidosii/MainActivity.java
@@ -1,28 +1,22 @@
 package com.proyecto.sistembebidosii;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.support.v7.app.ActionBarActivity;
 
 
-public class MainActivity extends Activity {
-
-
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
       public boolean onOptionsMenu(Menu menu) {
-
           getMenuInflater().inflate(R.menu.menu_main, menu);
-
         return true;
         }
 
@@ -46,8 +40,6 @@ public class MainActivity extends Activity {
         public void lanzar (View view) {
             Intent i = new Intent (this, Activity2.class);
             startActivity(i);
-
-
         }
 
 
