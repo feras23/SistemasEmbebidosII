c45b6710d591113984fe604280e49be9a6db20a6
Miguelonches <miguel.memc@gmail.com>
2015-02-21 03:25:44 -0600

    cambios

--- SistEmbebidosII/app/src/main/java/com/proyecto/sistembebidosii/Settings.java
+++ SistEmbebidosII/app/src/main/java/com/proyecto/sistembebidosii/Settings.java
@@ -3,31 +3,23 @@ package com.proyecto.sistembebidosii;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.content.Intent;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    public void openSettings(View view) {
-        Intent intent = new Intent(this, Settings.class);
-    }
+public class Settings extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
     }
 
     @Override
@@ -44,5 +36,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
