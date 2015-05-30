f77c324c636dc770f351b422faffa72ac20136af
FanniiveeGa <x_fanny_x@live.com.mx>
2015-05-30 00:35:53 -0500

    Ajustes - Vibrar

--- SistEmbebidosII/app/src/main/java/com/proyecto/sistembebidosii/OpcionesActivity.java
+++ SistEmbebidosII/app/src/main/java/com/proyecto/sistembebidosii/OpcionesActivity.java
@@ -1,27 +1,44 @@
 package com.proyecto.sistembebidosii;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
 
-/**
- * Created by miguel on 16/05/2015.
- */
+
 public class OpcionesActivity extends PreferenceActivity {
 
     public SharedPreferences prefs;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    Vibrator v;
 
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
-
         addPreferencesFromResource(R.xml.opciones);
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        final CheckBoxPreference checkboxPref = (CheckBoxPreference) getPreferenceManager().findPreference("vibrar");//key name in preference xml
+
+        checkboxPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener()
+        {
+            public boolean onPreferenceChange(Preference preference, Object newValue)
+            {
+                if (newValue.toString().equals("true"))
+                {
+                    v.vibrate(1000);
+                }
+                return true;
+            }
+        });
     }
-
-
 
 
 }
